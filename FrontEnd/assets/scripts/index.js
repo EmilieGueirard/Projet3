e28ba0abad8255f6343d372482f1015d27332218
Emilie Gueirard <emilie.gueirard@live.fr>
2024-05-28 21:55:35 +0200

    Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const url = "http://localhost:5678/api/works";
 const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
 // Récupération des travaux de l'API
 async function httpGet(uri) 
@@ -43,10 +44,46 @@ function showWorks(works)
     });
 }
 
+// Affichage du menu des catégories 
+function showCategories(works) {
+    const categoriesMap = new Map();
+    works.forEach(work => {
+        if (work.category && work.category.id && work.category.name) {
+            categoriesMap.set(work.category.id, work.category.name);
+        }
+    });
+   
+    const categoriesArray = Array.from(categoriesMap.entries()).sort((a, b) => a[0] - b[0]);
+
+    const categories = [["All", "Tous"], ...categoriesArray];
+
+    filters.innerHTML = "";
+    categories.forEach(([id, name]) => {
+        let button = document.createElement("button");
+        button.innerText = name; 
+        button.addEventListener("click", () => filterWorks(id, works)); 
+        filters.appendChild(button); 
+    });
+}
+
+
+// Filtrer les travaux par catégories
+function filterWorks(categoryId, works) {
+    if (categoryId === "All") {
+        showWorks(works); 
+    } else {
+        const filteredWorks = works.filter(work => work.category && work.category.id === categoryId);
+        showWorks(filteredWorks);
+    }
+}
+
+
+// Récupération et affichage des travaux et des catégories
 (async function() {
     let works = await httpGet(url);
     
     if (works) {
         showWorks(works);
+        showCategories(works)
     } 
 })();
