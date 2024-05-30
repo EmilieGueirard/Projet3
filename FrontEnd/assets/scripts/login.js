37f950b31e7f3747993facf13e81ccaf6a8b762d
Emilie Gueirard <emilie.gueirard@live.fr>
2024-05-30 13:12:17 +0200

    Création login.js + étapes en commentaire pour avoir un formulaire de connexion fonctionnel

---
+++ FrontEnd/assets/scripts/login.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const form = document.getElementById("loginForm");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+    
+// Ecouteur d'événement pour la soumission du formulaire
+
+// Gestion de la soumission du formulaire
+
+// Valider les champs du formulaire
+
+// Afficher les messages d'erreur sous les champs du formulaire
+
+// Récupérer les valeurs du formulaire
+
+// Authentification de l'utilisateur
+
+// Traitement de la réponse de l'API
+
+// Stockage du token et redirection vers la page d'accueil
