79863931254e6de529090d614d5fb0a8cafeccfc
Emilie Gueirard <emilie.gueirard@live.fr>
2024-06-07 17:21:37 +0200

    Ajout d'une modale pour la confirmation de suppression des projets + style en CSS

--- FrontEnd/assets/scripts/modal.js
+++ FrontEnd/assets/scripts/modal.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Écouteur d'événement pour DOMContentLoaded afin de s'assurer que le script s'exécute après le chargement complet du DOM
 document.addEventListener("DOMContentLoaded", () => {
     const modal = document.querySelector(".modal");
     const modifyLink = document.querySelector(".modify-link");
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     addInitialEventListeners(modal, modifyLink);
 });
 
+// Crée la modale1 : Galerie Photo
 function createGalleryModal(modal) {
     modal.innerHTML = ""; 
 
@@ -45,6 +47,7 @@ function createGalleryModal(modal) {
     modalWorks();
 }
 
+// Crée la modale2 : Ajoute une photo
 function createAddPhotoModal(modal) {
     modal.innerHTML = ""; 
 
@@ -68,19 +71,75 @@ function createAddPhotoModal(modal) {
     modalContent.appendChild(backButton);
     modal.appendChild(modalContent);
 
+    // Ajout des écouteurs d'événements pour la modale d'ajout de photo
     addCloseModalListener(closeButton, modal);
     addOpenGalleryModalListener(backButton, modal);
     addOutsideClickListener(modal);
 }
 
+// Crée la modale de confirmation de suppression
+function createDeleteConfirmationModal(modal, workId) {
+    const confirmationModal = document.createElement("div");
+    confirmationModal.classList.add("confirmation-modal");
+
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content3");
+
+    const modalTitle = document.createElement("h3");
+    modalTitle.classList.add("modalTitle");
+    modalTitle.textContent = "Confirmer la suppression";
+
+    const message = document.createElement("p");
+    message.textContent = "Voulez-vous vraiment supprimer ce projet ? Cette action est irréversible.";
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+
+    const cancelButton = document.createElement("button");
+    cancelButton.textContent = "Annuler";
+    cancelButton.classList.add("cancelButton");
+
+    const confirmButton = document.createElement("button");
+    confirmButton.textContent = "Confirmer";
+    confirmButton.classList.add("confirmButton");
+
+    buttonContainer.appendChild(cancelButton);
+    buttonContainer.appendChild(confirmButton);
+    modalContent.appendChild(modalTitle);
+    modalContent.appendChild(message);
+    modalContent.appendChild(buttonContainer);
+    confirmationModal.appendChild(modalContent);
+    modal.appendChild(confirmationModal);
+
+    // Ajout des écouteurs d'événements
+    cancelButton.addEventListener("click", () => {
+        confirmationModal.remove(); 
+    });
+
+    confirmButton.addEventListener("click", async () => {
+        await deleteWorkById(workId);
+        confirmationModal.remove(); 
+        createGalleryModal(modal); 
+        openModal(modal); 
+    });
+
+    // Empêche la fermeture de la modale de galerie en cliquant à l'extérieur de la modale de confirmation
+    confirmationModal.addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
+}
+
+// Ouvre la modale
 function openModal(modal) {
     modal.style.display = "flex";
 }
 
+// Ferme la modale
 function closeModal(modal) {
     modal.style.display = "none";
 }
 
+// Ajoute un écouteur d'événement pour ouvrir la modale de galerie photo
 function addOpenModalListener(link, modal) {
     link.addEventListener("click", (event) => {
         event.preventDefault();
@@ -89,18 +148,22 @@ function addOpenModalListener(link, modal) {
     });
 }
 
+// Ajoute un écouteur d'événement pour fermer la modale
 function addCloseModalListener(button, modal) {
     button.addEventListener("click", () => closeModal(modal));
 }
 
+// Ajoute un écouteur d'événement pour fermer la modale en cliquant à l'extérieur de celle-ci
 function addOutsideClickListener(modal) {
     window.addEventListener("click", (event) => {
-        if (event.target === modal) {
+        const confirmationModal = document.querySelector(".confirmation-modal");
+        if (event.target === modal && !confirmationModal) {
             closeModal(modal);
         }
     });
 }
 
+// Ajoute un écouteur d'événement pour ouvrir la modale d'ajout de photo
 function addOpenAddPhotoModalListener(button, modal) {
     button.addEventListener("click", (event) => {
         event.preventDefault();
@@ -108,6 +171,7 @@ function addOpenAddPhotoModalListener(button, modal) {
     });
 }
 
+// Ajoute un écouteur d'événement pour retourner à la galerie photo
 function addOpenGalleryModalListener(button, modal) {
     button.addEventListener("click", (event) => {
         event.preventDefault();
@@ -115,6 +179,7 @@ function addOpenGalleryModalListener(button, modal) {
     });
 }
 
+// Charge et affiche les travaux dans la modale de galerie photo
 async function modalWorks() {
     try {
         const response = await fetch("http://localhost:5678/api/works");
@@ -126,6 +191,7 @@ async function modalWorks() {
     }
 }
 
+// Réinitialise le contenu de la galerie photo dans la modale
 function resetModalGallery() {
     const modalGallery = document.querySelector(".worksModal1");
     if (modalGallery) {
@@ -135,6 +201,7 @@ function resetModalGallery() {
     }
 }
 
+// Crée et ajoute un élément de travail (article) dans la galerie photo
 function createModalGallery(work) {
     const modalGallery = document.querySelector(".worksModal1");
     if (!modalGallery) return; 
@@ -152,26 +219,20 @@ function createModalGallery(work) {
     modalGallery.appendChild(article);
 }
 
+// Crée une icône de poubelle (trash icon) pour chaque élément de travail
 function createTrashIcon(workId) {
     const trashIcon = document.createElement("i");
     trashIcon.classList.add("fa-solid", "fa-trash-can");
-    trashIcon.addEventListener("click", handleTrashIconClick.bind(null, workId));
+    trashIcon.addEventListener("click", (event) => {
+        event.preventDefault();
+        const modal = document.querySelector(".modal");
+        createDeleteConfirmationModal(modal, workId);
+        openModal(modal);
+    });
     return trashIcon;
 }
 
-function handleTrashIconClick(workId, e) {
-    e.preventDefault();
-    if (confirmDeletion()) {
-         setTimeout(() => {
-            deleteWorkById(workId);
-        }, 0);
-    }
-}
-
-function confirmDeletion() {
-    return confirm("Voulez-vous vraiment supprimer ce projet ?\nAttention ! Cette action est irréversible.");
-}
-
+// Supprime un travail par son ID
 async function deleteWorkById(workId) {
     try {
         await deleteWork(workId);
@@ -181,6 +242,7 @@ async function deleteWorkById(workId) {
     }
 }
 
+// Supprime l'élément de travail du DOM
 function deleteWorkFromDOM(workId) {
     const workElement = document.querySelector(`article[data-work-id="${workId}"]`);
     if (workElement) {
@@ -188,6 +250,7 @@ function deleteWorkFromDOM(workId) {
     }
 }
 
+// Récupère le token d'authentification depuis le localStorage
 function getToken() {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -196,6 +259,7 @@ function getToken() {
     return token;
 }
 
+// Effectue une requête pour supprimer un travail via l'API
 async function deleteWork(workId) {
     try {
         const token = getToken();
@@ -217,6 +281,7 @@ async function deleteWork(workId) {
     }
 }
 
+// Ajoute les écouteurs d'événements initiaux à la page
 function addInitialEventListeners(modal, modifyLink) {
     addOpenModalListener(modifyLink, modal);
     addOutsideClickListener(modal);
